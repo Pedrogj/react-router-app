6197846c69e5a6b8d36524ae033d6a8ca924c4dd
Pedro Perez <pedrodrums2015@gmail.com>
2025-11-22 11:12:36 -0300

    Add Navbar component

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 import { BrowserRouter } from "react-router";
 import { AppRouter } from "./AppRouter/AppRouter";
+import { Navbar } from "./components/Navbar";
 
 export const App = () => {
   return (
     <BrowserRouter>
+      <Navbar />
       <AppRouter />
     </BrowserRouter>
   );
